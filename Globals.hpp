c60b951b4208cbe27b25f1f9200e820674da857e
ZechariahWang <97078224+ZechariahWang@users.noreply.github.com>
2022-02-19 22:19:53 -0700

    Create Globals.hpp

---
+++ Globals.hpp
@@ -0,0 +1,32 @@
+#include "main.h"
+
+// Motors
+
+extern pros::Motor LeftLift;
+extern pros::Motor RightLift;
+
+
+extern pros::Motor DriveFrontLeft;
+extern pros::Motor DriveBackLeft;
+extern pros::Motor DriveFrontRight;
+extern pros::Motor DriveBackRight;
+
+
+extern pros::Motor TransmissionLeft;
+extern pros::Motor TransmissionRight;
+
+// Sensors
+
+extern pros::Imu imu_sensor;
+extern pros::Imu imu_sensor2;
+
+
+//pneumatics
+
+extern pros::ADIDigitalOut TransmissionPiston;
+extern pros::ADIDigitalOut Tilter;
+extern pros::ADIDigitalOut Clamp;
+
+// Controller
+
+extern pros::Controller controller;
